695068eaf7e19e571845eb797b0484d190c40b73
baltika <110934101+baltikaa9@users.noreply.github.com>
2026-05-10 16:47:04 +0300

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.go
@@ -0,0 +1,35 @@
+func isAlphanumeric(b byte) bool {
+	return b >= 'a' && b <= 'z' || b >= 'A' && b <= 'Z' || b >= '0' && b <= '9'
+}
+
+func isPalindrome(s string) bool {
+	if len(s) == 0 {
+		return true
+	}
+
+	s = strings.ToLower(s)
+
+	l := 0
+	r := len(s) - 1
+
+	for r >= l {
+		for !isAlphanumeric(s[l]) && l < len(s)-1 {
+			l++
+		}
+
+		for !isAlphanumeric(s[r]) && r > 0 {
+			r--
+		}
+
+		if r >= l {
+			if s[l] != s[r] {
+				return false
+			}
+		}
+
+		l++
+		r--
+	}
+
+	return true
+}
